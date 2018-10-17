e1dd1fe9cc0cb198e3b8a40297dc8e416a88dcec
Louwtjie <louwtjie8@hotmail.com>
2018-10-17 20:08:52 +0800

    Added a home page and todo page in my portfolio

---
+++ _site/todo/assets/main.js
@@ -0,0 +1,70 @@
+$todoVue = new Vue({
+  el: '.todo',
+  data: {
+    header: 'Todo',
+    task: '',
+    list: []
+  },
+  methods: {
+    newTask(e){
+      e.preventDefault();
+
+      if(this.task.length)
+      {
+        this.list.push(this.task);
+
+        this.task = '';
+      }
+
+      // this.toggleNewTask();
+    },
+    removeTask(task){
+      // Remove task from list
+      this.list.splice( task, 1 );
+    },
+    checkTask(e){
+      // Defualts
+      var $selected   = $(e.srcElement),
+          $text       = $selected.text(),
+          $target     = $selected.closest('div').children('p'),
+          $checked    = $target.attr('checkedTask'),
+          $html       = $checked === 'false'? $text: '<s>'+$text+'</s>';
+
+      // Check if it's the trash
+      if($selected.closest('button'))
+      {
+        return false;
+      }
+
+      // Set the content of the task
+      $target.html($html);
+
+      // Switching checked state
+      $target.attr('checkedTask', $checked === 'true'? false : true);
+    },
+    toggleNewTask(e){
+      var $this = this
+          $target = $('#newTask');
+
+      // Toggle new task and register blur
+      if($target.is( ":visible" ))
+      {
+        $target.slideUp();
+        $('#addTask').slideDown();
+      }
+      else
+      {
+        $target.slideDown()
+                      .find('input')
+                        .focus()
+                          .blur(
+                            function(evn){
+                              $this.toggleNewTask();
+                            }
+                          );
+
+        $('#addTask').slideUp();
+      }
+    }
+  }
+});
